36345417f621fdde5f55448424c8fb4eebce5a97
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2022-10-19 09:33:08 -0400

    finished working draft of scripts ha and hb (CONUS annual stats)

--- scripts/hpc/h_generate_annual_summary_plots.sh
+++ scripts/hpc/h_generate_annual_summary_plots.sh
@@ -12,6 +12,8 @@ module purge
 module load gcc openmpi eccodes anaconda
 source activate mrms_analysis
 
-echo "Running python script to create csv at gages and netcdf spanning gages for year ${year}..."
-python "scripts/_work_h_generate_annual_statistics_plots_to_qaqc.py"  "data/mrms_for_rainyday/${year}*.nc" "data/shapefiles/rain_gages.shp" "data/mrms_for_rainyday_subset_norfolk_csvs/${year}.csv" "data/mrms_for_rainyday_subset_norfolk_netcdfs/${year}.nc"
-echo "Script complete."
+echo "Running python script to create mrms_daily_totals.nc and mrms_yearly_totals.nc..."
+python "scripts/_ha_generate_annual_statistics_netcdfs.py" "data/mrms_for_rainyday_daily_consolidated/*.nc" 'data/mrms_daily_totals.nc' 'data/mrms_yearly_totals.nc' '_scratch_zarrs/h_yearly.zarr' "data/shapefiles/States_shapefile.shp" "plots/h_annual_statistics/"
+echo "First script complete. Running script to generate plots of annual totals and annual anomolies."
+python "scripts/_hb_generate_annual_statistics_plots_to_qaqc.py" 'data/mrms_daily_totals.nc' 'data/mrms_yearly_totals.nc' "data/shapefiles/States_shapefile.shp" "plots/h_annual_statistics/"
+echo "Script finished."
