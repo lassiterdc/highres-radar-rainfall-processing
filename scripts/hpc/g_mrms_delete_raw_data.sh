eaccb169cf86223326a515e06dbabcc4be280526
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2022-10-31 15:50:10 -0400

    removed unnecessary print statements

--- scripts/hpc/g_mrms_delete_raw_data.sh
+++ scripts/hpc/g_mrms_delete_raw_data.sh
@@ -13,21 +13,21 @@ cd ${assar_dirs[repo]}
 
 if [ ${SLURM_ARRAY_TASK_ID} = 1 ]
 then
-  echo "removing nssl gribs"
+  # echo "removing nssl gribs"
   rm -rf ${assar_dirs[raw_nssl]}
   echo "removed nssl gribs"
 fi
 
 if [ ${SLURM_ARRAY_TASK_ID} = 2 ]
 then
-  echo "removing mesonet gribs"
+  # echo "removing mesonet gribs"
   rm -rf ${assar_dirs[raw_mrms]}
   echo "removed mesonet gribs"
 fi
 
 if [ ${SLURM_ARRAY_TASK_ID} = 3 ]
 then
-  echo "removing mesonet netcdfs"
+  # echo "removing mesonet netcdfs"
   rm -rf ${assar_dirs[raw_mrms_quantized]}
   echo "removed mesonet netcdfs"
 fi
