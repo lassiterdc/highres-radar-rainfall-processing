11ff7a773708a23bfae81f29dce295fa5507fa04
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2022-11-18 12:07:34 -0500

    updated to also capture cancellations

--- scripts/hpc/_evaluate_errors.py
+++ scripts/hpc/_evaluate_errors.py
@@ -1,12 +1,15 @@
 #%%
 import glob
+import numpy as np
+#%%
 script_name = str(sys.argv[1])
 job_id = str(sys.argv[2])
 
 #%% testing
 script_name = "c_download_mrms_mesonet.sh"
+job_id = "45102273"
 fpath = "_script_errors/" + script_name
-job_id = "44848142"
+
 #%%
 files = glob.glob(fpath + "/" + job_id + "*")
 
@@ -18,7 +21,12 @@ for f in files:
     file = open(f, 'r')
     lines = file.readlines()
     for line in lines:
+        problem=False
         if "DUE TO TIME LIMIT" in line:
+            problem=True
+        if "CANCELLED AT" in line:
+            problem=True
+        if problem==True:
             failed_files.append(f)
             details = f.split(job_id)[-1].split("_")
             task = details[1]
@@ -34,3 +42,11 @@ for f in files:
 
 str_tasks = ','.join(str(item) for item in failed_tasks)
 str_nodes = ','.join(str(item) for item in problem_nodes)
+
+# documented excluded nodes so far
+excluded_nodes = "udc-aw29-25b,udc-an33-5c0,udc-an33-7c1,udc-aw29-19b,udc-an33-11c1,udc-aw34-3c0,udc-ba26-34c1,udc-aw34-4c0,udc-ba25-32c1,udc-aw29-23a,udc-aw34-19c0,udc-aw34-11c1"
+excluded_nodes = excluded_nodes.split(',')
+num_excluded = len(excluded_nodes)
+num_unique = len(np.unique(excluded_nodes))
+if num_excluded != num_unique:
+    print("Redundant nodes in list")
