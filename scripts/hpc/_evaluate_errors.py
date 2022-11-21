a75226515abc58fbc405ef4d46dbcbd28cc831e6
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2022-11-21 13:44:49 -0500

    updating download script to include only failed tasks

--- scripts/hpc/_evaluate_errors.py
+++ scripts/hpc/_evaluate_errors.py
@@ -1,13 +1,15 @@
 #%%
+# script_name = str(sys.argv[1])
+# job_id = str(sys.argv[2])
+
+#%% import libraries
 import glob
 import numpy as np
-#%%
-script_name = str(sys.argv[1])
-job_id = str(sys.argv[2])
+
 
 #%% testing
 script_name = "c_download_mrms_mesonet.sh"
-job_id = "45102273"
+job_id = "45124858"
 fpath = "_script_errors/" + script_name
 
 #%%
@@ -16,6 +18,7 @@ files = glob.glob(fpath + "/" + job_id + "*")
 failed_files = []
 failed_tasks = []
 problem_nodes = []
+problems = []
 
 for f in files:
     file = open(f, 'r')
@@ -24,8 +27,10 @@ for f in files:
         problem=False
         if "DUE TO TIME LIMIT" in line:
             problem=True
+            problems.append("FAILED DUE TO TIME LIMIT")
         if "CANCELLED AT" in line:
             problem=True
+            problems.append("CANCELLED")
         if problem==True:
             failed_files.append(f)
             details = f.split(job_id)[-1].split("_")
@@ -44,7 +49,7 @@ str_tasks = ','.join(str(item) for item in failed_tasks)
 str_nodes = ','.join(str(item) for item in problem_nodes)
 
 # documented excluded nodes so far
-excluded_nodes = "udc-aw29-25b,udc-an33-5c0,udc-an33-7c1,udc-aw29-19b,udc-an33-11c1,udc-aw34-3c0,udc-ba26-34c1,udc-aw34-4c0,udc-ba25-32c1,udc-aw29-23a,udc-aw34-19c0,udc-aw34-11c1"
+excluded_nodes = "udc-aw29-25b,udc-an33-5c0,udc-an33-7c1,udc-aw29-19b,udc-an33-11c1,udc-aw34-3c0,udc-ba26-34c1,udc-aw34-4c0,udc-ba25-32c1,udc-aw29-23a,udc-aw34-19c0,udc-aw34-11c1, udc-aw34-3c1"
 excluded_nodes = excluded_nodes.split(',')
 num_excluded = len(excluded_nodes)
 num_unique = len(np.unique(excluded_nodes))
--- scripts/hpc/c_download_mrms_mesonet.sh
+++ scripts/hpc/c_download_mrms_mesonet.sh
@@ -5,10 +5,10 @@
 #SBATCH -p standard				# Queue name "standard" (serial)
 #SBATCH -A quinnlab_paid				# allocation name
 #SBATCH -t 72:00:00				# Run time per serial job (hh:mm:ss)
-#SBATCH --array=168,169,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,253,254,255,256,257,258,293,294,295,296,297,298,299,300,301,302,303,304,305,306,307,308,309,312,313,314,315,316,317,318,319,320,321,322,324,325,326,327,328,329,349,350,351,352,353,354,355,356
+#SBATCH --array=295,296,297,298,299,300,301,302,303,304,305,306,307,308,309,312,313,314,315,316,317,318,319,320
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
 #SBATCH --mail-type=ALL   
-#SBATCH --exclude=udc-aw29-25b,udc-an33-5c0,udc-an33-7c1,udc-aw29-19b,udc-an33-11c1,udc-aw34-3c0,udc-ba26-34c1,udc-aw34-4c0,udc-ba25-32c1,udc-aw29-23a,udc-aw34-19c0,udc-aw34-11c1
+#SBATCH --exclude=udc-aw29-25b,udc-an33-5c0,udc-an33-7c1,udc-aw29-19b,udc-an33-11c1,udc-aw34-3c0,udc-ba26-34c1,udc-aw34-4c0,udc-ba25-32c1,udc-aw29-23a,udc-aw34-19c0,udc-aw34-11c1, udc-aw34-3c1
 									
 # add %20 after #SBATCH --array=1-366 if this is the first passthrough
 
