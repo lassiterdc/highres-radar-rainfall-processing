b452fd9bd4d6b686d4b22d3ba04154cd25b0c81b
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-05-24 13:31:34 -0400

    troubleshooting script because plots didn't make sense

--- scripts/hpc/_ha2_generate_annual_statistics_netcdfs_stageIV.py
+++ scripts/hpc/_ha2_generate_annual_statistics_netcdfs_stageIV.py
@@ -27,10 +27,11 @@ chnk_lon = int(round(num_lons / chnks_per_dim))
 
 #%% load input parameters
 # work
-f_in_nc = "/project/quinnlab/dcl3nd/norfolk/stormy/data/climate/StageIV_rainfall/" + "*/*.nc" # str(sys.argv[1])
+f_in_nc = "/project/quinnlab/dcl3nd/norfolk/stormy/data/climate/StageIV_rainfall/" + "2003/*20030501*.nc" # str(sys.argv[1])
 f_out_nc_yearlyavg = "/project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/data/stageiv_nc_preciprate_yearly_singlefile.nc" # str(sys.argv[2])
 fl_out_zar = "/project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/data/_scratch/zarrs/" + 'ha2_yearly.zarr' # str(sys.argv[3])
 fl_states = "/project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/data/geospatial/States_shapefile.shp" # str(sys.argv[4]) 
+fldr_plots = "/project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/plots/h_annual_statistics/" + "{}.png" # str(sys.argv[4]) + "{}.png"
 # end work
 
 f_in_nc = str(sys.argv[1]) + "*/*.nc" # "/project/quinnlab/dcl3nd/norfolk/stormy/data/climate/StageIV_rainfall/"
@@ -44,19 +45,53 @@ files.sort()
 lst_ds = []
 days = []
 
+# strt = time.time()
 # for f in tqdm(files):
-for f in files:
-    ds = xr.open_dataset(f, chunks={"latitude":chnk_lat, "longitude":chnk_lon}, engine='h5netcdf')
-    # ds = ds.sortby(["time"])
-    ds = remove_vars(ds)
-    lst_ds.append(ds)
-    days.append(len(ds.time))
+# # for f in files:
+#     ds = xr.open_dataset(f, chunks={"latitude":chnk_lat, "longitude":chnk_lon}, engine='h5netcdf')
+#     # ds = ds.sortby(["time"])
+#     ds = remove_vars(ds)
+#     lst_ds.append(ds)
+#     days.append(len(ds.time))
+#     end = time.time()
+# print("Created netcdf of annual averages. Script runtime: {}".format(end - strt))
+# ds_allyrs = xr.concat(lst_ds, dim="time", coords='minimal')
+# ds_allyrs = ds_allyrs.sortby(["time"])
 
-ds_allyrs = xr.concat(lst_ds, dim="time", coords='minimal')
-ds_allyrs = ds_allyrs.sortby(["time"])
+
+# strt = time.time()
+ds_allyrs = xr.open_mfdataset(files, engine = "h5netcdf")
+# convert from preceding to following time interval
+ds_allyrs["time"] = ds_allyrs.time - pd.Timedelta(1, "h")
+
+## convert any negative values to nan
+ds_allyrs = ds_allyrs.where(ds_allyrs["rainrate"]>=0)
+
+## convert any values above 9000 to nan
+ds_allyrs = ds_allyrs.where(ds_allyrs["rainrate"]<9000)
+
+# print("Created netcdf of annual averages. Script runtime: {}".format(time.time() - strt))
+
+#%% work - test plotting
+# ds_allyrs.rainrate.isel(time=[0,1]).plot.pcolormesh(x='outlon', y='outlat', col="time",
+#                                    robust=False, cmap='jet')
+#                                 #    vmin = 0, vmax = 1800)
+#                                    #cbar_kwargs={"label":"This scale is colored according to the 2nd and 98th percentiles."})
+# plt.savefig(fldr_plots.format("stage_iv_test1"), dpi=300)
+#%%
+
+
 #%% resample to year
 # https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#time-date-components
 ds_yearly = ds_allyrs.resample(time='Y').mean(skipna=True) 
+
+#%% work - test plotting
+# ds_yearly.rainrate.plot.pcolormesh(x='outlon', y='outlat', col="time",
+#                                    robust=False, cmap='jet')
+#                                 #    vmin = 0, vmax = 1800)
+#                                    #cbar_kwargs={"label":"This scale is colored according to the 2nd and 98th percentiles."})
+# plt.savefig(fldr_plots.format("stage_iv_test1"), dpi=300)
+#%%
 
 dtime = pd.DatetimeIndex(ds_yearly.time.values, freq='Y').year
 
@@ -67,12 +102,13 @@ ds_yearly.rainrate.attrs["short_name"] = "avg_yearly_precip"
 ds_yearly.rainrate.attrs["units"] = "mm/year"
 ds_yearly.rainrate.attrs["description"] = "Radar average yearly precipitation"
 
-ds_yearly.attrs = ds.attrs
+# ds_yearly.attrs = ds.attrs
 
-ds_yearly = ds_yearly.chunk(chunks={"outlat":chnk_lon, "outlon":chnk_lat, "time":1})
+# ds_yearly = ds_yearly.chunk(chunks={"outlat":chnk_lon, "outlon":chnk_lat, "time":1})
 
 ind_time = []
 lst_ds = []
+
 # convert from mm/day to mm/year
 for yr in ds_yearly.time.values:
     if yr % 4 == 0: # it is a leap year
