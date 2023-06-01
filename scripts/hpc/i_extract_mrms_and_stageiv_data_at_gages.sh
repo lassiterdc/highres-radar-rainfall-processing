9d57f9e9291647c1773f92ec1fdd50bee05e581c
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-06-01 16:59:29 -0400

    temp - reprocessing year 2011

--- scripts/hpc/i_extract_mrms_and_stageiv_data_at_gages.sh
+++ scripts/hpc/i_extract_mrms_and_stageiv_data_at_gages.sh
@@ -6,7 +6,7 @@
 #SBATCH -A quinnlab_paid				# allocation name
 #SBATCH -t 10:00:00				# Run time per serial job (hh:mm:ss)
 #SBATCH --mem=50000
-#SBATCH --array=1-22			# Array of jobs to loop through (2001-2022)
+#SBATCH --array=11			# Array of jobs to loop through (2001-2022)
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
 #SBATCH --mail-type=ALL   
 
