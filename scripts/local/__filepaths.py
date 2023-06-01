858f8138ad7f4d64046cc935b3bbf135fbd218be
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-06-01 17:02:31 -0400

    updating local filepaths for processing gridded precip data at gages

--- scripts/local/__filepaths.py
+++ scripts/local/__filepaths.py
@@ -2,15 +2,18 @@
 fldr_stageIV_data = "D:/GDrive/grad_school/StageIV_rainfall/{}/{}.nc" # (year, file name)
 fldr_data = "D:/Dropbox/_GradSchool/_norfolk/highres-radar-rainfall-processing/data/"
 f_ncs_fullres = fldr_data + "mrms_nc_preciprate_fullres_yearlyfiles_atgages/*.nc"
+f_ncs_fullres_stageiv = fldr_data + "stage_iv_nc_preciprate_fullres_yearlyfiles_atgages/*.nc"
 f_nc_yearly = fldr_data + "mrms_nc_preciprate_yearly_singlefile.nc"
 
 f_nc_atgages = fldr_data + "mrms_nc_preciprate_fullres_atgages.nc"
 f_nc_24h_atgages = fldr_data + "mrms_nc_preciprate_24h_atgages.nc"
 f_nc_1M_atgages = fldr_data + "mrms_nc_preciprate_monthly_atgages.nc"
 
-f_csvs_fullres = fldr_data + "mrms_csv_preciprate_fullres_yearlyfiles_atgages/*.csv"
+f_mrms_csvs_fullres = fldr_data + "mrms_csv_preciprate_fullres_yearlyfiles_atgages/*.csv"
+f_stageiv_csvs_fullres = fldr_data + "stage_iv_csv_preciprate_fullres_yearlyfiles_atgages/*.csv"
 fl_events = fldr_data + "gage_event_data/rainfall_events_all.csv"
 f_csv_mrms_and_gage_events = fldr_data + "/gage_and_mrms_csv_event_preciprate.csv"
+f_csv_stageiv_and_gage_events = fldr_data + "/gage_and_stageiv_csv_event_preciprate.csv"
 
 
 # f_in_nc_24h = "D:/mrms_processing/out_netcdfs/a1_mrms_at_norfolk_24h.nc"
@@ -31,9 +34,10 @@ f_nexrad_station_shp = fldr_nexrad_data + "nexrad_stations.shp"
 
 # filepaths for _work scripts
 ## work_a
-fld_mrms_hourly = "D:/Dropbox/_GradSchool/_norfolk/highres-radar-rainfall-processing/data/mrms_nc_preciprate_hourly_dailyfiles/"
+fld_mrms_hourly = fldr_data + "mrms_nc_preciprate_hourly_dailyfiles/"
 f_stormcat_mrms_hrly_2003 = "D:/Dropbox/_GradSchool/_norfolk/stormy/stochastic_storm_transposition/norfolk/norfolk_mrms_sst_subset_short_2003_hourly_rivanna.nc"
-
+## work b
+f_nc_yearly_stageIV = fldr_data + "stageiv_nc_preciprate_yearly_singlefile.nc"
 
 
 #%% filepaths for script a1
@@ -41,7 +45,7 @@ def return_a1_filepaths():
     return f_ncs_fullres, f_nc_atgages, f_nc_24h_atgages, f_nc_1M_atgages
 
 def return_a2_filepaths():
-    return f_csvs_fullres, fl_events, f_csv_mrms_and_gage_events
+    return f_mrms_csvs_fullres, f_stageiv_csvs_fullres, fl_events, f_csv_mrms_and_gage_events, f_csv_stageiv_and_gage_events
 
 def return_b_filepaths():
     return f_nc_24h_atgages, f_nc_atgages, f_csv_mrms_and_gage_events, fldr_out_plots, f_shp_coast, f_shp_subcatchments, f_shp_gages
@@ -49,8 +53,11 @@ def return_b_filepaths():
 def return_c_filepaths():
     return url_stations, fldr_nexrad_data, f_station_data_with_dates, f_stations, f_nexrad_station_shp
 
+def return_d_filepaths():
+    return f_nc_24h_atgages, f_nc_atgages, fldr_stageIV_data, f_csv_mrms_and_gage_events, f_csv_stageiv_and_gage_events, fldr_out_plots, f_shp_coast, f_shp_subcatchments, f_shp_gages
+
 def return_worka_filepaths():
     return fld_mrms_hourly, f_stormcat_mrms_hrly_2003
 
 def return_workb_filepaths():
-    return fldr_stageIV_data, f_nc_yearly
+    return f_nc_yearly, f_nc_yearly_stageIV
