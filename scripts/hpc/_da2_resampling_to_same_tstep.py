9d1f756936a5b5bc49a79e2f43bf95e2120f0df3
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-07-10 14:16:54 -0400

    tbs

--- scripts/hpc/_da2_resampling_to_same_tstep.py
+++ scripts/hpc/_da2_resampling_to_same_tstep.py
@@ -44,7 +44,7 @@ if "NULL" in in_date:
 fl_in_nc = fldr_in_nc_day +"{}.nc".format(in_date)
 fl_out_nc = fldr_out_nc +"{}.nc".format(in_date)
 
-print(fl_in_nc)
+# print(fl_in_nc)
 
 ds = xr.open_dataset(fl_in_nc, chunks = dict(latitude = chnk_sz))
 
