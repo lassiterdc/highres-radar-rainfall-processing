1413342275eafd234734d2d6edf930f53b9dd98c
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-07-13 13:51:32 -0400

    changing allocation to dev and reducing job time

--- scripts/hpc/da3_qaqc_resampling.sh
+++ scripts/hpc/da3_qaqc_resampling.sh
@@ -3,13 +3,13 @@
 #SBATCH -e _script_errors/%x/%A_%a_%N.out
 #SBATCH --ntasks=1				# Number of tasks per serial job (must be 1)
 #SBATCH -p standard				# Queue name "standard" (serial)
-#SBATCH -A quinnlab_paid				# allocation name
-#SBATCH -t 48:00:00				# Run time per serial job (hh:mm:ss)
+#SBATCH -A dev				# allocation name
+#SBATCH -t 1:00:00				# Run time per serial job (hh:mm:ss)
 #SBATCH --array=1			#  1-366 Array of jobs to loop through (366 days)
-#SBATCH --mem-per-cpu=80000
+# SBATCH --mem-per-cpu=80000
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
 #SBATCH --mail-type=ALL   
-#SBATCH --exclude=udc-ba26-18,udc-ba27-14,udc-ba26-16,udc-ba26-17
+# SBATCH --exclude=udc-ba26-18,udc-ba27-14,udc-ba26-16,udc-ba26-17
 
 module purge
 module load gcc openmpi eccodes anaconda # the stuff other than anaconda was to ensure eccodes loaded correctly
