d6714c4a2beb0b94e149d3a20cf900bed3a67c47
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-07-13 13:48:45 -0400

    revising to export a csv with performance statistics

--- scripts/hpc/_da2_resampling_to_same_tstep.py
+++ scripts/hpc/_da2_resampling_to_same_tstep.py
@@ -49,78 +49,63 @@ fldr_out_csv = str(sys.argv[5]) # ${assar_dirs[scratch_zarrs]} # "/project/quinn
 performance["date"] = in_date
 
 f_out_export_perf = fldr_out_zarr + "_export_stats_{}.csv".format(in_date)
-
-if "NULL" in in_date:
-    sys.exit("Failed to create netcdf for {}. No netcdf file created likely because the SLURM_ARRAY_TASK_ID is 366 on a non-leap year. This is expected.".format(in_date))
-
 #%% netcdf 
 fl_in_nc = fldr_in_nc_day +"{}.nc".format(in_date)
 fl_out_nc = fldr_out_nc +"{}.nc".format(in_date)
 fl_out_zarr = fldr_out_zarr +"{}.zarr".format(in_date)
 fl_out_csv = fldr_out_csv +"da2_resampling_{}.csv".format(in_date)
 
-performance["loaded_netcdf"] = False
-ds = xr.open_dataset(fl_in_nc, chunks = dict(longitude = chnk_sz))
-performance["loaded_netcdf"] = True
+performance["problem_loading_netcdf"] = False
+performance["loading_netcdf_errors"]  = "None"
+try:
+    ds = xr.open_dataset(fl_in_nc, chunks = dict(longitude = chnk_sz))
+except Exception as e:
+    performance["loading_netcdf_errors"]  = e
+    performance["problem_loading_netcdf"] = True
+    
 
 # tstep = ds.attrs["time_step"]
-tstep_min = pd.to_timedelta(ds.attrs["time_step"]).total_seconds() / 60
-
-num_tsteps = ds.coords["time"].shape[0]
-
-duration_h = num_tsteps * tstep_min / 60
-
-performance["duration_h"] = duration_h
-# verify the full day has coverage
-if duration_h != 24:
-    sys.exit("Failed to create netcdf for {}. Duration does not add up to 24 hours.".format(in_date))
-#%% testing 
-# da = xr.DataArray(np.arange(100), coords=[pd.date_range("2020-01-01", periods = 100, freq = "2T")], dims = "time")
-
-# t_idx_1min = pd.date_range("2020-01-01", periods = 200, freq='1min')
-
-# da_1min = da.reindex(dict(time = t_idx_1min)).ffill(dim="time")
-
-# da_5min = da_1min.resample(time = "5T").mean()
-
-#%% resampling
-# da = xr.DataArray(np.arange(100), coords=[pd.date_range("2020-01-01", periods = 100, freq = "2T")], dims = "time")
-performance["resampled"] = False
-t_idx_1min = pd.date_range(ds.time.values[0], periods = 24*60, freq='1min')
-
-ds_1min = ds.reindex(dict(time = t_idx_1min)).ffill(dim="time")
-
-# ds_1min = ds_1min.chunk(chunks = dict(latitude = chnk_sz))  
-
-# ds_1min = ds_1min.unify_chunks()
-
-da_target = ds_1min.resample(time = "{}Min".format(target_tstep)).mean()
-performance["resampled"] = True
-# da_target = da_target.chunk(chunks = dict(longitude = chnk_sz))  
-
-# da_target = da_target.unify_chunks()
-
-# WORK
-import numpy as np
-da_target = da_target.isel(dict(time = np.arange(100, 110)))
-# END WORK
-#%% export to zarr, then export to netcdf (this was found to be faster than a direct export to netcdf)
-performance["to_netcdf_success"] = True
-performance["to_netcdf_problems"] = "None"
-try:
-    da_target.to_zarr(fl_out_zarr, mode="w")
-    ds_from_zarr = xr.open_zarr(store=fl_out_zarr, chunks={'time':chnk_sz})
-    ds_from_zarr.to_netcdf(fl_out_nc, encoding= {"rainrate":{"zlib":True}})
-    shutil.rmtree(fl_out_zarr)
-except Exception as e:
-    print("Simulation failed due to error: {}".format(e))
-    performance["to_netcdf_problems"]  = e
-    performance["to_netcdf_success"] = False
-    sys.exit("Exporting netcdf for {} failed.".format(in_date))
-
-df = pd.DataFrame(performance)
+if performance["problem_loading_netcdf"] == False:
+    # verify the full day has coverage
+    tstep_min = pd.to_timedelta(ds.attrs["time_step"]).total_seconds() / 60
+    num_tsteps = ds.coords["time"].shape[0]
+    duration_h = num_tsteps * tstep_min / 60
+    performance["duration_h"] = duration_h
+    performance["problem_with_duration"] = False
+    if duration_h != 24:
+        performance["problem_with_duration"] = True
+    # resampling
+    performance["problems_resampling"] = True
+    t_idx_1min = pd.date_range(ds.time.values[0], periods = 24*60, freq='1min')
+    ds_1min = ds.reindex(dict(time = t_idx_1min)).ffill(dim="time")
+    da_target = ds_1min.resample(time = "{}Min".format(target_tstep)).mean()
+    performance["problems_resampling"] = False
+    # WORK
+    # import numpy as np
+    # da_target = da_target.isel(dict(time = np.arange(100, 110)))
+    # END WORK
+    # export to zarr, then export to netcdf (this was found to be faster than a direct export to netcdf)
+    # export to zarr
+    performance["problem_exporting_zarr"] = False
+    performance["to_zarr_errors"] = "None"
+    try:
+        da_target.to_zarr(fl_out_zarr, mode="w")
+        ds_from_zarr = xr.open_zarr(store=fl_out_zarr, chunks={'time':chnk_sz})
+    except Exception as e:
+        performance["to_zarr_errors"]  = e
+        performance["problem_exporting_zarr"] = True
+    # export to netcdf
+    performance["problem_exporting_netcdf"] = False
+    performance["to_netcdf_errors"] = "None"
+    try:
+        ds_from_zarr.to_netcdf(fl_out_nc, encoding= {"rainrate":{"zlib":True}})
+        shutil.rmtree(fl_out_zarr)
+    except Exception as e:
+        # print("Simulation failed due to error: {}".format(e))
+        performance["to_netcdf_errors"]  = e
+        performance["problem_exporting_netcdf"] = True
+# export performance dictionary to a csv
+time_elapsed_min = round((time.time() - start_time) / 60, 2)
+performance["time_elapsed_min"] = time_elapsed_min
+df = pd.DataFrame(performance, index = [1])
 df.to_csv(fl_out_csv)
-#%%
-# da_target_loaded = da_target.load()
-
-# da_target_loaded.to_netcdf(fl_out_nc, encoding= {"rainrate":{"zlib":True}})
