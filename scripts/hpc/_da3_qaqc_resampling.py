77bb750a88c5ca9008cc0a110d47312e513bafc9
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-01-26 17:31:33 -0500

    cleaning up

--- scripts/hpc/_da3_qaqc_resampling.py
+++ scripts/hpc/_da3_qaqc_resampling.py
@@ -15,23 +15,15 @@ performance = {}
 # in_date = "20190306"
 
 # fldr_in_nc_day = "/project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/data/mrms_nc_preciprate_fullres_dailyfiles/"
-fldr_out_nc = "/scratch/dcl3nd/highres-radar-rainfall-processing/out_fullres_dailyfiles_consolidated/"
+# fldr_out_nc = "/scratch/dcl3nd/highres-radar-rainfall-processing/out_fullres_dailyfiles_consolidated/"
 # fldr_out_zarr = "/project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/data/_scratch/zarrs/"
-fldr_out_csv = "/project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/data/_scratch/csv/"
+# fldr_out_csv = "/project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/data/_scratch/csv/"
 #%% end work
+fldr_out_nc = str(sys.argv[1]) # ${assar_dirs[out_fullres_dailyfiles_consolidated]} # "/scratch/dcl3nd/highres-radar-rainfall-processing/out_fullres_dailyfiles_consolidated/"
+fldr_da2_csv = str(sys.argv[2])
 
-# folders (with proceeding fwd slash)
-# fldr_in_nc_day = str(sys.argv[1]) # ${assar_dirs[out_fullres_dailyfiles]} # "/project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/data/mrms_nc_preciprate_fullres_dailyfiles/"
-fldr_out_nc = str(sys.argv[2]) # ${assar_dirs[out_fullres_dailyfiles_consolidated]} # "/scratch/dcl3nd/highres-radar-rainfall-processing/out_fullres_dailyfiles_consolidated/"
-# fldr_out_zarr = str(sys.argv[3]) # ${assar_dirs[scratch_zarrs]} # "/project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/data/_scratch/zarrs/"
-fldr_in_csv = str(sys.argv[4]) # ${assar_dirs[scratch_zarrs]} # "/project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/data/_scratch/csv/"
-
-#%% netcdf 
-# fl_in_nc = fldr_in_nc_day +"{}.nc".format(in_date)
-# fl_out_nc = fldr_out_nc +"{}.nc".format(in_date)
-# fl_out_zarr = fldr_out_zarr +"{}.zarr".format(in_date)
-fl_out_csv = fldr_out_csv +"da2_resampling_{}.csv".format("*") # must match pattern in script da2
-lst_f_csvs = glob(fl_out_csv)
+fl_da2_csv = fldr_da2_csv +"da2_resampling_{}.csv".format("*") # must match pattern in script da2
+lst_f_csvs = glob(fl_da2_csv)
 
 lst_dfs = []
 for f in lst_f_csvs:
@@ -40,6 +32,3 @@ for f in lst_f_csvs:
 df = pd.concat(lst_dfs, ignore_index = True)
 
 df.to_csv(fldr_out_nc+"_da3_resampling_performance.csv")
-
-#%% work
-# df[(df.problem_loading_netcdf == False) & (df.current_tstep_different_than_target == True)].drop(["problem_loading_netcdf", "loading_netcdf_errors", "duration_h", "problem_with_duration", "current_tstep_different_than_target"], axis = 1)
