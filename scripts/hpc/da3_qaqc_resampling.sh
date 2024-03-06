63164f0d2de9c35d90d9024fb0cd66010c8d83f6
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-03-06 11:27:41 -0500

    decreasing memory allocation

--- scripts/hpc/da3_qaqc_resampling.sh
+++ scripts/hpc/da3_qaqc_resampling.sh
@@ -3,15 +3,15 @@
 #SBATCH -e _script_errors/%x/%A_%a_%N.out
 #SBATCH --ntasks=1				# Number of tasks per serial job (must be 1)
 #SBATCH -p standard				# Queue name "standard" (serial)
-#SBATCH -A quinnlab_paid				# allocation name
+#SBATCH -A quinnlab				# allocation name
 #SBATCH -t 1:00:00				# Run time per serial job (hh:mm:ss)
 #SBATCH --array=1			#  1-366 Array of jobs to loop through (366 days)
-# SBATCH --mem-per-cpu=80000
+#SBATCH --mem-per-cpu=16000
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
 #SBATCH --mail-type=ALL   
 # SBATCH --exclude=udc-ba26-18,udc-ba27-14,udc-ba26-16,udc-ba26-17
 
-# ijob -c 1 -A quinnlab -p standard --time=0-08:00:00 --mem-per-cpu=36000
+# ijob -c 1 -A quinnlab -p standard --time=0-08:00:00 --mem-per-cpu=16000
 
 module purge
 module load gcc openmpi eccodes anaconda # the stuff other than anaconda was to ensure eccodes loaded correctly
