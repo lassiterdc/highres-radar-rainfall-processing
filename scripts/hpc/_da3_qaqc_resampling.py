a881912b903beea1890b90bc54c8ba57dcd26d82
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-03-20 12:12:21 -0400

    debugging memory allocation errors

--- scripts/hpc/_da3_qaqc_resampling.py
+++ scripts/hpc/_da3_qaqc_resampling.py
@@ -33,9 +33,11 @@ for f in lst_f_csvs:
 
 df = pd.concat(lst_dfs, ignore_index = True)
 
-df_successes = df[df.problem_exporting_netcdf == False]
-
 df.to_csv(fldr_out_nc+"_da3_resampling_performance.csv")
+
+df = pd.read_csv(fldr_out_nc+"_da3_resampling_performance.csv")
+
+df_successes = df[df.problem_exporting_netcdf == False]
 
 # qaqc of dataset as a whole
 fl_csv_qaqc = fldr_csvs +"qaqc_of_daily_fullres_data_{}.csv".format("*") # must match pattern in script da2
@@ -56,6 +58,7 @@ for id, row in df_yearmonths.iterrows():
     bm_time = time.time()
     year = row.year
     month = row.month
+    fl_scratch_nc_qaqc = fldr_scratch_zarr + "da3_qaqc_{}{}.nc".format(year, month)
     lst_f_netcdfs = glob(fldr_out_nc + "{}{}*.nc".format(year, month))
     lst_f_netcdfs.sort()
     lst_ds_qaqc = []
@@ -79,7 +82,6 @@ for id, row in df_yearmonths.iterrows():
     d_encoding = {}
     for da_name in ds_qaqc.data_vars:
         d_encoding[da_name] = {"zlib":True}
-    fl_scratch_nc_qaqc = fldr_scratch_zarr + "da3_qaqc_{}{}.nc".format(year, month)
     ds_qaqc.load().to_netcdf(fl_scratch_nc_qaqc, encoding=d_encoding, engine="h5netcdf")
     time_elapsed_min = round((time.time() - bm_time) / 60, 2)
     # print("Exported temporary netcdf qaqc file for year and month {}-{}. Time to export: {}.".format(year, month, time_elapsed_min))
@@ -99,33 +101,35 @@ lst_ncs_year_st4_res = []
 print("Creating yearly netcdfs of qaqc data.....")
 lst_ncs_year = []
 for year in df_yearmonths.year.unique():
+    fl_nc_qaqc_out = fldr_out_nc + "_qaqc_of_resampled_data_{}.nc".format(year)
+    f_out_resampled = fl_nc_qaqc_out.split(".nc")[0] + "_st4_res.nc"
+    lst_ncs_year.append(fl_nc_qaqc_out)
+    lst_ncs_year_st4_res.append(f_out_resampled)
     bm_time = time.time()
     lst_f_scratch_nc = glob(fldr_scratch_zarr + "da3_qaqc_{}*.nc".format(year))
     ds_qaqc_year = xr.open_mfdataset(lst_f_scratch_nc, engine = "h5netcdf")
-    fl_nc_qaqc_out = fldr_out_nc + "_qaqc_of_resampled_data_{}.nc".format(year)
     d_encoding = {}
     for da_name in ds_qaqc_year.data_vars:
         d_encoding[da_name] = {"zlib":True}
     ds_qaqc_year.to_netcdf(fl_nc_qaqc_out, encoding=d_encoding, engine="h5netcdf")
     time_elapsed_min = round((time.time() - bm_time) / 60, 2)
-    lst_ncs_year.append(fl_nc_qaqc_out)
     print("Exported temp netcdf qaqc file for year {}. Time to export: {}.".format(year, time_elapsed_min))
     bm_time2 = time.time()
-    ds_qaqc_year = xr.open_dataset(fl_nc_qaqc_out, engine="h5netcdf")
+    ds_qaqc_year = xr.open_dataset(fl_nc_qaqc_out, engine="h5netcdf", chunks={"date":"auto"})
     ds_stageiv_subset = clip_ds_to_another_ds(ds_stageiv, ds_qaqc_year)
     ds_qaqc_year_to_st4 = spatial_resampling(ds_qaqc_year, ds_stageiv_subset, "latitude", "longitude", missingfillval = 0)
-    f_out_resampled = fl_nc_qaqc_out.split(".nc")[0] + "_st4_res.nc"
+    d_encoding = {}
     for da_name in ds_qaqc_year_to_st4.data_vars:
         d_encoding[da_name] = {"zlib":True}
     ds_qaqc_year_to_st4.to_netcdf(f_out_resampled, encoding=d_encoding, engine="h5netcdf")
-    lst_ncs_year_st4_res.append(f_out_resampled)
+    
     time_elapsed_min = round((time.time() - bm_time2) / 60, 2)
     print("Additional time to export temp netcdf consolidated to stage iv resolution: {}.".format(time_elapsed_min))
 print("Done creating yearly netcdfs of qaqc data.....")
 # finally combine them all into a single netcdf
 ## mrms spatial resolution
 bm_time = time.time()
-ds_qaqc_all = xr.open_mfdataset(lst_ncs_year, engine = "h5netcdf")
+ds_qaqc_all = xr.open_mfdataset(lst_ncs_year, engine = "h5netcdf", chunks=dict(date = "auto"))
 d_encoding = {}
 for da_name in ds_qaqc_all.data_vars:
     d_encoding[da_name] = {"zlib":True}
@@ -140,12 +144,12 @@ for f in (lst_f_scratch_nc+lst_ncs_year):
 
 ## stage iv spatial resolution
 bm_time = time.time()
-ds_qaqc_all = xr.open_mfdataset(lst_ncs_year_st4_res, engine = "h5netcdf")
+ds_qaqc_all_st4_res = xr.open_mfdataset(lst_ncs_year_st4_res, engine = "h5netcdf", chunks = dict(date = "auto"))
 d_encoding = {}
-for da_name in ds_qaqc_all.data_vars:
+for da_name in ds_qaqc_all_st4_res.data_vars:
     d_encoding[da_name] = {"zlib":True}
 
-ds_qaqc_all.to_netcdf(fl_nc_qaqc_out_all_st4_res, encoding=d_encoding, engine="h5netcdf")
+ds_qaqc_all_st4_res.to_netcdf(fl_nc_qaqc_out_all_st4_res, encoding=d_encoding, engine="h5netcdf")
 time_elapsed_min = round((time.time() - bm_time) / 60, 2)
 print("Exported netcdf qaqc file for entire dataset. Time to export: {}.".format(time_elapsed_min))
 print("filepath exported: {}".format(fl_nc_qaqc_out_all_st4_res))
