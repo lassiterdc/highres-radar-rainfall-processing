20feea73fb2ba19facb08e972381efbdc1de9cc3
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-09-30 13:48:30 -0400

    processing new years

--- scripts/hpc/da_cmbn_to_dly_ncs_frmtd_for_RainyDay.sh
+++ scripts/hpc/da_cmbn_to_dly_ncs_frmtd_for_RainyDay.sh
@@ -3,13 +3,25 @@
 #SBATCH -e _script_errors/%x/%A_%a_%N.out
 #SBATCH --ntasks=1				# Number of tasks per serial job (must be 1)
 #SBATCH -p standard				# Queue name "standard" (serial)
-#SBATCH -A quinnlab_paid				# allocation name
+#SBATCH -A quinnlab				# allocation name
 #SBATCH -t 48:00:00				# Run time per serial job (hh:mm:ss)
-#SBATCH --array=10,11,12,3,4,5,7,8,9			#  1-366 Array of jobs to loop through (366 days)
+#SBATCH --array=1-366			#  1-366 Array of jobs to loop through (366 days)
+#SBATCH -c 1
 #SBATCH --mem-per-cpu=80000
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
 #SBATCH --mail-type=ALL   
-#SBATCH --exclude=udc-ba26-18,udc-ba27-14,udc-ba26-16,udc-ba26-17
+# SBATCH --exclude=udc-ba26-18,udc-ba27-14,udc-ba26-16,udc-ba26-17
+
+# ijob -A quinnlab -p standard --time=0-08:00:00 -c 1 --mem-per-cpu=80000
+# --time=0-08:00:00 -c 1 --mem-per-cpu=80000, SLURM_ARRAY_TASK_ID=105, YEAR = 2023
+# --time=0-08:00:00 -c 4 --mem-per-cpu=20000, SLURM_ARRAY_TASK_ID=105, YEAR = 2015
+# made some changes (Exporting zarr earlier in process)
+# -c 2 --mem-per-cpu=80000 took forever
+
+# cd /project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/scripts/hpc
+# rm /project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/scripts/hpc/da_cmbn_to_dly_ncs_frmtd_for_RainyDay.sh
+# rm /project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/scripts/hpc/_da_cmbn_to_dly_ncs_frmtd_for_RainyDay.py
+# git pull
 
 module purge
 module load gcc openmpi eccodes anaconda # the stuff other than anaconda was to ensure eccodes loaded correctly
@@ -17,23 +29,40 @@ source activate mrms_processing
 
 source __utils.sh
 source __directories.sh
-# move to working directory
-cd ${assar_dirs[repo]}
 
-# all years, hours and minutes to loop through for each day of the year
-YEARS=$(seq 2001 2022)
+#%% work
+# YEAR=2023
+# SLURM_ARRAY_TASK_ID=105
+# determine_month_and_day ${YEAR} ${SLURM_ARRAY_TASK_ID}
+# month=${array_out[0]}
+# day=${array_out[1]}
+# echo ${YEAR}${month}${day}
+# python ${assar_dirs[hpc_da]} ${YEAR}${month}${day} ${assar_dirs[raw_mrms]} ${assar_dirs[raw_nssl]} ${assar_dirs[raw_mrms_quantized]} ${assar_dirs[scratch_zarrs]} ${assar_dirs[scratch_gribs]} ${assar_dirs[out_fullres_dailyfiles]}
 
+# # testing out alternative approach to combining grib files into netcdf
+# conda activate grib_tools
+# start_time=$(date +%s)
+# echo "writing ${assar_dirs[scratch_gribs]}${YEAR}${month}${day}.grib2"
+# # write directly to netcdf
+# cdo -O -f nc mergetime ${assar_dirs[scratch_gribs]}${YEAR}${month}${day}.grib2 ${assar_dirs[out_fullres_dailyfiles]}${YEAR}${month}${day}.nc
+# # Time taken: 5.95 minutes
+# end_time=$(date +%s)
+# elapsed_time=$((end_time - start_time))
+# elapsed_minutes=$(echo "scale=2; $elapsed_time / 60" | bc)
+# echo "Time taken: $elapsed_minutes minutes"
+#%% end work
+
+YEARS=$(seq 2022 2024)
 # loop through all years
 for YEAR in ${YEARS}
 do
-	year=${YEAR}
 	determine_month_and_day ${YEAR} ${SLURM_ARRAY_TASK_ID}
 	month=${array_out[0]}
 	day=${array_out[1]}
 	# process the mrms mesonet grib data
 	# echo "Node ID: $HOSTNAME"
 	# echo "Slurm Array Task ID: ${SLURM_ARRAY_TASK_ID}"
-	python ${assar_dirs[hpc_da]}  ${year}${month}${day} ${assar_dirs[raw_mrms]} ${assar_dirs[raw_nssl]} ${assar_dirs[raw_mrms_quantized]} ${assar_dirs[scratch_zarrs]} ${assar_dirs[scratch_gribs]} ${assar_dirs[out_fullres_dailyfiles]}
-	# echo "Finished attempt to create netcdf for ${year}${month}${day}"
+	python ${assar_dirs[hpc_da]}  ${YEAR}${month}${day} ${assar_dirs[raw_mrms]} ${assar_dirs[raw_nssl]} ${assar_dirs[raw_mrms_quantized]} ${assar_dirs[scratch_zarrs]} ${assar_dirs[scratch_gribs]} ${assar_dirs[out_fullres_dailyfiles]}
+	# echo "Finished attempt to create netcdf for ${YEAR}${month}${day}"
 done
 
