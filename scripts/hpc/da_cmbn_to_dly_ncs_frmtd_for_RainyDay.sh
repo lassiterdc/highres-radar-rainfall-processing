5f46dfc1ac1ff2a5f6a825fcabb4a33352bc2c5c
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-10-10 13:30:30 -0400

    decreasing memory allocation

--- scripts/hpc/da_cmbn_to_dly_ncs_frmtd_for_RainyDay.sh
+++ scripts/hpc/da_cmbn_to_dly_ncs_frmtd_for_RainyDay.sh
@@ -7,21 +7,24 @@
 #SBATCH -t 72:00:00				# Run time per serial job (hh:mm:ss)
 #SBATCH --array=1-366			#  1-366 Array of jobs to loop through (366 days)
 #SBATCH -c 1
-#SBATCH --mem-per-cpu=80000
+#SBATCH --mem-per-cpu=9000
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
 #SBATCH --mail-type=ALL   
 # SBATCH --exclude=udc-ba26-18,udc-ba27-14,udc-ba26-16,udc-ba26-17
 
 # ijob -A quinnlab -p standard --time=0-08:00:00 -c 1 --mem-per-cpu=9000
+
+# ijob -A quinnlab -p standard --time=0-08:00:00 -c 1 --mem-per-cpu=80000 | ymd=20200406 | 8.51 minutes
+# ijob -A quinnlab -p standard --time=0-08:00:00 -c 4 --mem-per-cpu=20000 | ymd=20210406 | 8.87 minutes
+# ijob -A quinnlab -p standard --time=0-08:00:00 -c 8 --mem-per-cpu=9000 | ymd=20220406 | 
+# ijob -A quinnlab -p standard --time=0-08:00:00 -c 1 --mem-per-cpu=9000 | ymd=20230406 | 20 minutes
+# python ${assar_dirs[hpc_da]} $ymd ${assar_dirs[raw_mrms]} ${assar_dirs[raw_nssl]} ${assar_dirs[raw_mrms_quantized]} ${assar_dirs[scratch_zarrs]} ${assar_dirs[scratch_gribs]} ${assar_dirs[out_fullres_dailyfiles]}
+
 # -c 1 --mem-per-cpu=9000 | 15 minutes
 # --time=0-08:00:00 -c 1 --mem-per-cpu=80000, SLURM_ARRAY_TASK_ID=105, YEAR = 2023 exporting to zarr | 14.91 minutes
 # made some changes (Exporting zarr earlier in process)
 # -c 2 --mem-per-cpu=80000 took forever
 
-# cd /project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/scripts/hpc
-
-# running batch script
-# sbatch da_cmbn_to_dly_ncs_frmtd_for_RainyDay.sh
 
 # inspecting errors
 # cd /project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/scripts/hpc/_script_errors/da_cmbn_to_dly_ncs_frmtd_for_RainyDay.sh/
@@ -37,35 +40,13 @@
 # rm /project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/scripts/hpc/_script_outputs/da_cmbn_to_dly_ncs_frmtd_for_RainyDay.sh/*
 # rm /project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/scripts/hpc/_script_errors/da_cmbn_to_dly_ncs_frmtd_for_RainyDay.sh/*
 
-
+# cd /project/quinnlab/dcl3nd/norfolk/highres-radar-rainfall-processing/scripts/hpc
 module purge
 module load gcc openmpi eccodes anaconda # the stuff other than anaconda was to ensure eccodes loaded correctly
 source activate mrms_processing
 
 source __utils.sh
 source __directories.sh
-
-#%% work
-# YEAR=2023
-# SLURM_ARRAY_TASK_ID=105
-# determine_month_and_day ${YEAR} ${SLURM_ARRAY_TASK_ID}
-# month=${array_out[0]}
-# day=${array_out[1]}
-# echo ${YEAR}${month}${day}
-# python ${assar_dirs[hpc_da]} ${YEAR}${month}${day} ${assar_dirs[raw_mrms]} ${assar_dirs[raw_nssl]} ${assar_dirs[raw_mrms_quantized]} ${assar_dirs[scratch_zarrs]} ${assar_dirs[scratch_gribs]} ${assar_dirs[out_fullres_dailyfiles]}
-
-# # testing out alternative approach to combining grib files into netcdf
-# conda activate grib_tools
-# start_time=$(date +%s)
-# echo "writing ${assar_dirs[scratch_gribs]}${YEAR}${month}${day}.grib2"
-# # write directly to netcdf
-# cdo -O -f nc mergetime ${assar_dirs[scratch_gribs]}${YEAR}${month}${day}.grib2 ${assar_dirs[out_fullres_dailyfiles]}${YEAR}${month}${day}.nc
-# # Time taken: 5.95 minutes
-# end_time=$(date +%s)
-# elapsed_time=$((end_time - start_time))
-# elapsed_minutes=$(echo "scale=2; $elapsed_time / 60" | bc)
-# echo "Time taken: $elapsed_minutes minutes"
-#%% end work
 
 YEARS=$(seq 2001 2024)
 # loop through all years
