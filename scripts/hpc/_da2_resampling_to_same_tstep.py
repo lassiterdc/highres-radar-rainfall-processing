fe21935529db9c733b4ab5a7e55862edfec7ed4c
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-10-17 09:42:18 -0400

    tst

--- scripts/hpc/_da2_resampling_to_same_tstep.py
+++ scripts/hpc/_da2_resampling_to_same_tstep.py
@@ -30,12 +30,15 @@ target_tstep_min = 2
 
 tsteps_per_day = int(24 * 60 / target_tstep_min)
 tsteps_per_hr = 60 / target_tstep_min
-final_chunking_dict = dict(time = tsteps_per_day, latitude = 3, longitude = 3)
+final_chunking_dict = dict(time = tsteps_per_day/2, latitude = 1, longitude = 1)
 # WITH exporting penultimate zarr
-# final_chunking_dict = dict(time = tsteps_per_day, latitude = 4, longitude = 4)
-# final_chunking_dict = dict(time = tsteps_per_day, latitude = 3, longitude = 3)
-# final_chunking_dict = dict(time = tsteps_per_day, latitude = 1, longitude = 1)
-# final_chunking_dict = dict(time = tsteps_per_day, latitude = 500, longitude = 500) | killeds
+# final_chunking_dict = dict(time = tsteps_per_day/2, latitude = 5, longitude = 5)
+# final_chunking_dict = dict(time = tsteps_per_day/2, latitude = 3, longitude = 3)
+# final_chunking_dict = dict(time = tsteps_per_day/2, latitude = 1, longitude = 1)
+# final_chunking_dict = dict(time = tsteps_per_day, latitude = 4, longitude = 4) | killed
+# final_chunking_dict = dict(time = tsteps_per_day, latitude = 3, longitude = 3) | killed
+# final_chunking_dict = dict(time = tsteps_per_day, latitude = 1, longitude = 1) | killed
+# final_chunking_dict = dict(time = tsteps_per_day, latitude = 500, longitude = 500) | killed
 # final_chunking_dict = dict(time = tsteps_per_day, latitude = 50, longitude = 50) | killed
 # final_chunking_dict = dict(time = tsteps_per_day, latitude = 5, longitude = 5) | killed
 # WITHOUT exporting penultimate zarr
