c1b8ce1dc18d47cf1995f20a13e72a49e6284f39
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-10-17 20:38:07 -0400

    tst

--- scripts/hpc/_da2_resampling_to_same_tstep.py
+++ scripts/hpc/_da2_resampling_to_same_tstep.py
@@ -30,7 +30,7 @@ target_tstep_min = 2
 
 tsteps_per_day = int(24 * 60 / target_tstep_min)
 tsteps_per_hr = 60 / target_tstep_min
-target_chunks_per_day = 0.5
+target_chunks_per_day = 0.25
 # final_chunking_dict = dict(time = tsteps_per_hr*1, latitude = 1, longitude = 1)
 # WITH exporting penultimate zarr
 # final_chunking_dict = dict(time = tsteps_per_hr*1, latitude = 5, longitude = 5)
